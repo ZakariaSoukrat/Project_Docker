cd64b3b78cb7adb5b3d321fd2ef9e32e92476bd6
BarbecueNote <tbrothers076@gmail.com>
2024-03-06 17:39:22 +0100

    Update vote1 and vote2 image references, votes working, seed still not

--- seed-data/generate-votes.sh
+++ seed-data/generate-votes.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 # create 3000 votes (2000 for option a, 1000 for option b)
-ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://nginx:80/
-ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" http://nginx:80/
-ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://nginx:80/
+#ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://nginx:80/
+#ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" http://nginx:80/
+#ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://nginx:80/
+
+ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote:5001/
+ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" http://vote:5001/
+ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote:5001/
 
 #changed 8000 to 80
